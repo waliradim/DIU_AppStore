0846cbaea736fac181ad94f710c11a1e743ee80a
Mahmud wali <waliradim@gmail.com>
2018-04-02 22:41:17 +0600

    Coudse display

--- AppStore/App_Start/BundleConfig.cs
+++ AppStore/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace AppStore
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                       "~/Style/StyleSheet.css"));
         }
     }
 }
